95c999aac7d82edc1fb8c531d00b06f6af74507d
Abhishek Singh <abhiwebdev2718@gmail.com>
2023-03-03 13:03:16 +0530

    connecting with db

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express"),
   port = process.env.port || 1010,
   path = require("path"),
+  mongoose = require("mongoose"),
   cors = require("cors"),
   postRoutes = require("./routes/post"),
   errorHandler = require("./middlewares/errorHandler"),
   app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/rest-api-node", {
+  useNewUrlParser: true,
+});
 
 app.use(cors());
 app.use(express.json());
